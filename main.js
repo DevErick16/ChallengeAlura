841952e1d0da06b218853c8982640474cdbe14fd
Erick <erick_mora@hotmail.es>
2023-04-21 16:14:05 -0500

    creando funciones encriptar, desencriptar, y accionar y modificando .html

--- main.js
+++ main.js
@@ -1,21 +1,64 @@
-const llaves = {
-    e: "enter",
-    i: "imes",
-    a: "ai",
-    o: "ober",
-    u: "ufat",
+const llaves ={
+    e:"enter",
+    i:"imes",
+    a:"ai",
+    o:"ober",
+    u:"ufat"
 }
 
-function encriptar (texto){
+function encrypt (text) {
 
-    return replace(/[aeiou]/g, function(match){
-        return llaves[match]
+    return text.replace(/[aeiou]/g, function(match){
+        return llaves[match];
     });
 
 }
 
-function desencriptar (texto){
+function decrypt (text) {
 
-    return replace(/enter/g,"e").replace(/imes/g,"i").replace(/ai/g,"a").replace(/ober/g,"o").replace(/ufat/g,"u")
+    return text
+                .replace(/enter/g,"e")
+                .replace(/imes/g,"i")
+                .replace(/ai/g,"a")
+                .replace(/ober/g,"o")
+                .replace(/ufat/g,"u");
+            
+}
+
+function triggerEncrypt (){
+    let input = document.getElementById("textareainput");
+    text = input.value;
+    let output = document.getElementById("textareaoutput");
+
+    if(text == ""){
+        alert("No hay ningun texto por Encriptar")
+    }else{
+
+        let resultEncrypt = encrypt(text);
+        output.value = resultEncrypt;
+
+    }
 
 }
+
+function triggerDecrypt (){
+    let input = document.getElementById("textareainput");
+    text = input.value;
+    let output = document.getElementById("textareaoutput");
+
+    if(text == ""){
+        alert("No hay ningun texto por Desencriptar")
+    }else{
+
+        let resultDecrypt = decrypt(text);
+        output.value = resultDecrypt;
+
+    }
+
+}
+
+let buttonEncrypt = document.getElementById("btnEncrypt");
+buttonEncrypt.onclick = triggerEncrypt;
+
+let buttonDecrypt = document.getElementById("btnDecrypt");
+buttonDecrypt.onclick = triggerDecrypt;
