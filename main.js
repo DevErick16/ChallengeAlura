c4c7989743da2296b94bd7abf46f244c77b961bf
Erick <erick_mora@hotmail.es>
2023-04-21 14:24:51 -0500

    Creacion de funciones pára encriptar y desencriptar

--- main.js
+++ main.js
@@ -5,3 +5,17 @@ const llaves = {
     o: "ober",
     u: "ufat",
 }
+
+function encriptar (texto){
+
+    return replace(/[aeiou]/g, function(match){
+        return llaves[match]
+    });
+
+}
+
+function desencriptar (texto){
+
+    return replace(/enter/g,"e").replace(/imes/g,"i").replace(/ai/g,"a").replace(/ober/g,"o").replace(/ufat/g,"u")
+
+}
